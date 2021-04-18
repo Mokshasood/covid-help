c09799806e4dd3a36bef774b4b6b98f46647e63f
Mokshasood <53305803+Mokshasood@users.noreply.github.com>
2021-04-18 20:03:56 +0530

    Update essentials.component.js

--- src/components/essentials.component.js
+++ src/components/essentials.component.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Data from '../data.json';
 
 const Essentials = () => {
-	return (
-	<h1> hi </h1>
-	);
-};
+  const [searchTerm, setSearchTerm] = useState('')
+  return (
+    <div class="container">
+    <h3> Helpline Numbers </h3>
+    <input type="text" placeholder="Search state..." onChange={(e) => {setSearchTerm(e.target.value);}} />
+    <br/>
+    <table class="table table-bordered">
+    <tr>
+      <th> State </th>
+      <th> Helpline Number </th>
+    </tr>
+      {Data.filter((val) => {
+        if (searchTerm==""){
+          return val
+        } else if (val.loc.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val
+        }
+      }).map(post => {
+        return (
+          <tr>
+          <td style={{width:"50%"}}> {post.loc} </td>
+          <td style={{width:"50%"}}> {post.number} </td>
+          </tr>
+          
+          )
+      })}
+      </table>
+    </div>);
+}
 
 export default Essentials;
