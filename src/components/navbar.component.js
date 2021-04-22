61a5262104c7dec63ddd27b79f3689a9e4576915
Mokshasood <53305803+Mokshasood@users.noreply.github.com>
2021-04-22 09:44:59 +0530

    Update navbar.component.js

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import {Navbar, Nav, Dropdown, NavDropdown } from 'react-bootstrap';
 
 const NavBar = () => {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar expanded={expanded} collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand><Link to="/" className="navbar-brand">
         COVID TRACKER</Link>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link><Link to="/home" className="nav-link">Home</Link></Nav.Link>
-          <Nav.Link><Link to="/stats" className="nav-link">Status</Link></Nav.Link>
-          <Nav.Link><Link to="/precautions" className="nav-link">Precautions</Link></Nav.Link>
-          <Nav.Link><Link to="/essentials" className="nav-link">Essentials</Link></Nav.Link>
+          <Nav.Link onClick={() => setExpanded(false)}><Link to="/home" className="nav-link">Home</Link></Nav.Link>
+          <Nav.Link onClick={() => setExpanded(false)}><Link to="/stats" className="nav-link">Status</Link></Nav.Link>
+          <Nav.Link onClick={() => setExpanded(false)}><Link to="/precautions" className="nav-link">Precautions</Link></Nav.Link>
+          <Nav.Link onClick={() => setExpanded(false)}><Link to="/essentials" className="nav-link">Essentials</Link></Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
